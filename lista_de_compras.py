4b071bf6d8fe3e22a369845801401ff95dec2c03
vitor-nery11 <vitornerysantos24@gmail.com>
2026-05-08 00:30:29 -0300

    fix: consertando erros

--- lista_de_compras.py
+++ lista_de_compras.py
@@ -1,3 +1,16 @@
+"""
+Faça uma lista de comprar com listas
+O usuário deve ter a possibilidade de
+inserir, apagar e listar valores da sua lista
+Não permita que o programa quebre com 
+erros de índices inexistentes na lista.
+"""
+
+# minha resolução 
+
+
+import os
+
 print('\n')
 print('bem vindo a sua lista de compras')
 
@@ -10,19 +23,25 @@ while True:
   print('3. listar produtos')
   print('4. sair')
   print('\n')
+
   escolha = int(input('Digite a sua escolha:'))
 
   if escolha == 1:
+      os.system('clear')
       lista_compras.append(input('digite o que deseja adicionar:'))
+
   elif escolha == 2:
-      for indice,item in enumerate(lista_compras):
+      for indice,item in enumerate(lista_compras, start=1):
           print(indice, item)
       lista_compras.pop(int(input('digite o numero do que deseja remover:')))
       print(lista_compras)
+  
   elif escolha == 3:
-      for indice,item in enumerate(lista_compras):
-          print('\n')
+      os.system('clear')
+      for indice,item in enumerate(lista_compras, start=1): 
           print(indice, item)
+      print('\n')
+
   elif escolha == 4:
       print('obrigado por usar a nossa lista!!')
       break
